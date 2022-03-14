1d70c3c198017c122014907f4a945c7d4b4704a7
Jackie Macharia <jacklinewmacharia@gmail.com>
2022-03-14 14:23:39 +0300

    Test scheduled functions

---
+++ netlify/functions/hello-world-scheduled.js
@@ -0,0 +1,11 @@
+const { schedule } = require('@netlify/functions')
+
+
+const handler = async (event, context) => {
+    console.log('Received event:', event, 'Time: ', new Date().toISOString());
+    return {
+      statusCode: 200
+    };
+  };
+
+  module.exports.handler = schedule('@hourly', handler);
